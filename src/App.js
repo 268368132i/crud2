4dc617b6b604373cdbaceb8171e12f1fe42f0c8e
Ilya <ilya@mylap>
2022-06-04 21:31:14 +0400

    This is a new CRUD project. This time I'm paying more attention to efficiency.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router,Routes, Route } from "react-router-dom";
+import Auditories from "./Auditories";
+import Auditory from "./Auditory";
+import { NewItem } from "./ItemForm";
+import { ItemsList } from "./ItemsList";
+import MyNavbar from "./MyNavbar";
+import { Welcome } from "./Welcome";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <MyNavbar />
+        <Container>
+          <Routes>
+            <Route path="/auditory/all" element={<Auditories />} />
+            <Route path="/auditory/:id/details" element={<Auditory />} />
+            <Route path="/auditory/new" element={<Auditory isNew={true}/>} />
+            <Route path="/item/new" element={<NewItem isNew={true}/>} />
+            <Route path="/item/all" element={<ItemsList/>} />
+
+            <Route path="/" element={<Welcome />} />
+
+          </Routes>
+        </Container>
+
+      </div>
+    </Router>
   );
 }
 
